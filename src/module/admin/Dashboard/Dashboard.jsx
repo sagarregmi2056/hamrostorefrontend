e16632ea96178248a4b2162d166c102a29af82ae
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2023-12-02 20:42:32 +0545

    dashboard

--- src/module/admin/Dashboard/Dashboard.jsx
+++ src/module/admin/Dashboard/Dashboard.jsx
@@ -1,7 +1,20 @@
+import Sidebar from "../sidebar/Sidebar";
 import React from "react";
+// import Dashboard from './Dashboard';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      {/* side bar */}
+      <div className="flex">
+        <div>
+          <Sidebar />
+        </div>
+
+        <div className="flex-1 p-8">Dashboard</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
